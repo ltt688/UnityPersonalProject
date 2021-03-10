363ea2b7fbe404ea01d45d1f05fc2cdab0b2ded3
roast_racoon <798175437@qq.com>
2021-03-10 14:19:31 +0800

    add background and obstacle texture
    add a new asset
    add background
    add obstacle texture

---
+++ PersonalProject/Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private GameManager gameManager;
+    private Vector3 initPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        initPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.isGameActive)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (transform.position.x < -12.4)
+            {
+                transform.position = initPos;
+            }
+        }
+    }
+}
