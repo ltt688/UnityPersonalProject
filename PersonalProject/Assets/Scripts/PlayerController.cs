84ad20cebb9cf46ecda82d25b20d90af13cae379
Hncwt9 <hncwt9@umsystem.edu>
2021-03-11 14:23:41 -0600

    Initial non-working script for playing animation on touch/keypress

--- PersonalProject/Assets/Scripts/PlayerController.cs
+++ PersonalProject/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 250.0f;
+    //adjust value to tune flying
+    public float gravityModifier = 1.0f;
+    //not working
+    public Animator flyingAnimation;
+
     private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Physics.gravity *= gravityModifier;
+        flyingAnimation = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,12 +28,14 @@ public class PlayerController : MonoBehaviour
         if (up == true)
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+            //flyingAnimation.Play("Flying");
         }
         if (touch > 0)
         {
             if(Input.touches[0].phase == TouchPhase.Began)
             {
                 GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+                //flyingAnimation.Play("Flying");
             }
         }
     }
