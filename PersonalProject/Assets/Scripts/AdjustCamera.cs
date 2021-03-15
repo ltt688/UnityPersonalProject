49c85a31423e7b91f06c7d4dee3d7afe266c252b
Hncwt9 <hncwt9@umsystem.edu>
2021-03-14 23:27:19 -0500

    Adjust game camera width to android perspective
    did not get height perspective to work

---
+++ PersonalProject/Assets/Scripts/AdjustCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustCamera : MonoBehaviour
+{
+    public bool maintainWidth = true;
+
+    private float defaultWidth;
+    //private float defaultHeight;
+
+    //private Vector3 CameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CameraPos = Camera.main.transform.position;
+
+        defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        //defaultHeight = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maintainWidth)
+        {
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+            //Camera.main.transform.position = new Vector3(CameraPos.x, 1 * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+        }
+        else
+        {
+
+        }
+    }
+}
